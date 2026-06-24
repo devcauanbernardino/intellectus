8a3eeb0707b643b7fa89d76e144e2fba6a5cb60c
devcauanbernardino <devcauanbernardino@gmail.com>
2026-06-24 14:54:58 -0300

    inicio do projeto

---
+++ backend/src/main/java/com/intellectus/intellectus/IntellectusApplication.java
@@ -0,0 +1,13 @@
+package com.intellectus.intellectus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellectusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/intellectus/intellectus/IntellectusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellectus.intellectus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellectusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
